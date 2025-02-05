855c4794ab4e73cffc29c047a71433782be426ee
SHIVAPRASAD GOUD <106461790+Shivaprasad2426@users.noreply.github.com>
2025-02-05 16:55:31 +0530

    Add files via upload

---
+++ LargeJavaFile.java
@@ -0,0 +1,130 @@
+// LargeJavaFile.java
+
+// Package declaration (if needed)
+package com.example.largefile;
+
+import java.util.*;
+
+public class LargeJavaFile {
+
+    public static void main(String[] args) {
+        System.out.println("Starting Large Java File Example...");
+
+        // Example usage of some classes and methods
+        ClassA classA = new ClassA();
+        classA.methodA1();
+        classA.methodA2();
+
+        ClassB classB = new ClassB();
+        classB.methodB1();
+        classB.methodB2();
+
+        System.out.println("Large Java File Example Completed.");
+    }
+}
+
+class ClassA {
+    public void methodA1() {
+        System.out.println("ClassA - methodA1");
+    }
+
+    public void methodA2() {
+        System.out.println("ClassA - methodA2");
+    }
+}
+
+class ClassB {
+    public void methodB1() {
+        System.out.println("ClassB - methodB1");
+    }
+
+    public void methodB2() {
+        System.out.println("ClassB - methodB2");
+    }
+}
+
+// Adding more classes and methods to make the file larger
+
+class ClassC {
+    public void methodC1() {
+        System.out.println("ClassC - methodC1");
+    }
+
+    public void methodC2() {
+        System.out.println("ClassC - methodC2");
+    }
+}
+
+class ClassD {
+    public void methodD1() {
+        System.out.println("ClassD - methodD1");
+    }
+
+    public void methodD2() {
+        System.out.println("ClassD - methodD2");
+    }
+}
+
+class ClassE {
+    public void methodE1() {
+        System.out.println("ClassE - methodE1");
+    }
+
+    public void methodE2() {
+        System.out.println("ClassE - methodE2");
+    }
+}
+
+// Continue adding more classes and methods as needed to increase the size of the file
+
+class ClassF {
+    public void methodF1() {
+        System.out.println("ClassF - methodF1");
+    }
+
+    public void methodF2() {
+        System.out.println("ClassF - methodF2");
+    }
+}
+
+class ClassG {
+    public void methodG1() {
+        System.out.println("ClassG - methodG1");
+    }
+
+    public void methodG2() {
+        System.out.println("ClassG - methodG2");
+    }
+}
+
+class ClassH {
+    public void methodH1() {
+        System.out.println("ClassH - methodH1");
+    }
+
+    public void methodH2() {
+        System.out.println("ClassH - methodH2");
+    }
+}
+
+class ClassI {
+    public void methodI1() {
+        System.out.println("ClassI - methodI1");
+    }
+
+    public void methodI2() {
+        System.out.println("ClassI - methodI2");
+    }
+}
+
+class ClassJ {
+    public void methodJ1() {
+        System.out.println("ClassJ - methodJ1");
+    }
+
+    public void methodJ2() {
+        System.out.println("ClassJ - methodJ2");
+    }
+}
+
+// And so on...
