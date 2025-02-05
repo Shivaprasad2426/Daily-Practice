c9b2cf3348a05212ac321c59a0f5207c86418df6
SHIVAPRASAD GOUD <106461790+Shivaprasad2426@users.noreply.github.com>
2025-02-05 17:28:08 +0530

    Add files via upload

---
+++ large_python_file.py
@@ -0,0 +1,107 @@
+# large_python_file.py
+
+def function1():
+    print("Function 1")
+
+def function2():
+    print("Function 2")
+
+def function3():
+    print("Function 3")
+
+def function4():
+    print("Function 4")
+
+def function5():
+    print("Function 5")
+
+# Adding more functions to increase the size of the file
+
+def function6():
+    print("Function 6")
+
+def function7():
+    print("Function 7")
+
+def function8():
+    print("Function 8")
+
+def function9():
+    print("Function 9")
+
+def function10():
+    print("Function 10")
+
+class ClassA:
+    def methodA1(self):
+        print("ClassA - methodA1")
+
+    def methodA2(self):
+        print("ClassA - methodA2")
+
+class ClassB:
+    def methodB1(self):
+        print("ClassB - methodB1")
+
+    def methodB2(self):
+        print("ClassB - methodB2")
+
+# Adding more classes to increase the size of the file
+
+class ClassC:
+    def methodC1(self):
+        print("ClassC - methodC1")
+
+    def methodC2(self):
+        print("ClassC - methodC2")
+
+class ClassD:
+    def methodD1(self):
+        print("ClassD - methodD1")
+
+    def methodD2(self):
+        print("ClassD - methodD2")
+
+class ClassE:
+    def methodE1(self):
+        print("ClassE - methodE1")
+
+    def methodE2(self):
+        print("ClassE - methodE2")
+
+# Continue adding more functions and classes as needed to increase the size of the file
+
+def main():
+    function1()
+    function2()
+    function3()
+    function4()
+    function5()
+    function6()
+    function7()
+    function8()
+    function9()
+    function10()
+
+    classA = ClassA()
+    classA.methodA1()
+    classA.methodA2()
+
+    classB = ClassB()
+    classB.methodB1()
+    classB.methodB2()
+
+    classC = ClassC()
+    classC.methodC1()
+    classC.methodC2()
+
+    classD = ClassD()
+    classD.methodD1()
+    classD.methodD2()
+
+    classE = ClassE()
+    classE.methodE1()
+    classE.methodE2()
+
+if __name__ == "__main__":
+    main()
