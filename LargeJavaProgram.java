ddd1ce1bad32d87dc05284745533bc671dd5293f
SHIVAPRASAD GOUD <106461790+Shivaprasad2426@users.noreply.github.com>
2025-02-06 18:23:54 +0530

    Create LargeJavaProgram.java

---
+++ LargeJavaProgram.java
@@ -0,0 +1,33 @@
+public class LargeJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("This is a large Java program example.");
+
+        // Simulating a large file with a series of print statements
+        for (int i = 1; i <= 30; i++) {
+            System.out.println("Line number: " + i);
+        }
+
+        System.out.println("End of the large Java program.");
+    }
+
+    // Additional methods to increase the size of the file
+    public void method1() {
+        System.out.println("Method 1");
+    }
+
+    public void method2() {
+        System.out.println("Method 2");
+    }
+
+    public void method3() {
+        System.out.println("Method 3");
+    }
+
+    public void method4() {
+        System.out.println("Method 4");
+    }
+
+    public void method5() {
+        System.out.println("Method 5");
+    }
+}
