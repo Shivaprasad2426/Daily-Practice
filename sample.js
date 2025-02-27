ce0822ddb0a59ffc575657cd44cb36c8558f0295
SHIVAPRASAD GOUD <106461790+Shivaprasad2426@users.noreply.github.com>
2025-02-27 22:17:48 +0530

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,4 @@
+document.getElementById('change-message-button').addEventListener('click', function() {
+    var welcomeMessage = document.getElementById('welcome-message');
+    welcomeMessage.textContent = 'You have clicked the button!';
+});
